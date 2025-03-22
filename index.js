8282c9c83f8ce6568ff5f6f56a48699bf55becc1
IsaiahThao <chumengt@gmail.com>
2025-03-22 10:54:27 -0500

    Add the outline to the grid space. Also add code to dynamically change the size of squares within the grid.

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-userInput = 4;
+const userInput = 16;
+let width = Math.floor(420/userInput)
 container = document.querySelector('.container')
-for (let i =0; i < userInput; i++ ){
-    for (let j=0; j < userInput; j++ ){
-        const element = document.createElement('div')
-        element.id = 'grid'
-        container.appendChild(element)
+
+function createGrid(){
+    for (let i =0; i < userInput; i++ ){
+        for (let j=0; j < userInput; j++ ){
+            const element = document.createElement('div')
+            element.id = 'grid'
+            element.style.width=`${width}px`;
+            element.style.height=`${width}px`;
+            container.appendChild(element)
+        }
     }
 }
